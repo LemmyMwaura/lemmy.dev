5f514c38df396f61870f583d8a0be76e3e0896d3
lemmy mwaura <lemminton@gmail.com>
2023-01-15 18:00:10 +0300

    feat: add fetcher fn

---
+++ src/lib/getProfile.ts
@@ -0,0 +1,42 @@
+import { graphql, GraphQlQueryResponseData } from '@octokit/graphql'
+
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${import.meta.env.GITHUB_TOKEN}`,
+  },
+})
+
+const query = `{
+  user(login: "LemmyMwaura") {
+    pinnedItems(first: 6) {
+      totalCount
+      edges {
+        node {
+          ... on Repository {
+            id
+            name
+            forkCount
+            stargazerCount
+            url
+            languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  viewer {
+    avatarUrl
+    login
+  }
+}`
+
+const { user, viewer }: GraphQlQueryResponseData = await graphqlWithAuth(query)
+const pinneditems: GraphQlQueryResponseData = user.pinnedItems.edges.map(({ node }: any) => node)
+
+export { viewer, pinneditems }
