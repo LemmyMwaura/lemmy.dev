056d3f7290896d493cc7b70971e8449250eb5efe
lemmy mwaura <lemminton@gmail.com>
2023-01-16 21:34:27 +0300

    chore: uninstall react and astro icons - not needed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  integrations: [mdx()]
 });
