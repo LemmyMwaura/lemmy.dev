b1f22e50681e791763fab15f290936842318c755
lemmy mwaura <lemminton@gmail.com>
2023-01-17 19:24:07 +0300

    style: mini style update

--- src/lib/getProfile.ts
+++ src/lib/getProfile.ts
@@ -1,43 +1,47 @@
-import { graphql, GraphQlQueryResponseData } from '@octokit/graphql'
+import axios from 'axios';
+const URL = 'https://api.github.com/graphql';
 
-const graphqlWithAuth = graphql.defaults({
+const response = await axios(URL, {
+  method: 'post',
   headers: {
+    'Content-Type':'application/json',
     authorization: `token ${import.meta.env.GITHUB_TOKEN}`,
   },
-})
-
-const query = `{
-  user(login: "LemmyMwaura") {
-    pinnedItems(first: 6) {
-      totalCount
-      edges {
-        node {
-          ... on Repository {
-            id
-            name
-            description
-            forkCount
-            stargazerCount
-            url
-            languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
-              edges {
-                node {
-                  name
+  data: {
+    query: `query { user(login: "LemmyMwaura") {
+        pinnedItems(first: 6) {
+          totalCount
+          edges {
+            node {
+              ... on Repository {
+                id
+                name
+                description
+                forkCount
+                stargazerCount
+                url
+                languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
+                  edges {
+                    node {
+                      name
+                    }
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-  }
-  viewer {
-    avatarUrl
-    login
-  }
-}`
+      viewer {
+        avatarUrl
+        login
+      }}`,
+  },
+})
 
-const { user, viewer }: GraphQlQueryResponseData = await graphqlWithAuth(query)
-const pinneditems: GraphQlQueryResponseData = user.pinnedItems.edges.map(({ node }: any) => node)
+const { user, viewer }: any = response.data.data
+const pinneditems: any = user.pinnedItems.edges.map(
+  ({ node }: any) => node
+)
 
 export { viewer, pinneditems }
