98b4146b51f0364ea25d8c2c56e025a941fadcd2
lemmy mwaura <lemminton@gmail.com>
2023-01-17 16:04:36 +0300

    style: update faq

--- src/lib/faq.ts
+++ src/lib/faq.ts
@@ -5,18 +5,18 @@ export const FAQs = [
   },
   {
     question: "What's your view on Typesafety?",
-    answer: "I think good Typesafety is essential to writing amazing javaScript code and it should never be sacrifised",
+    answer: "I think good Typesafety is essential to writing amazing javaScript code and it should never be sacrifised.",
   },
   {
     question: "What are you working on nowadays?",
-    answer: "Currently contributing to Infisical(an Open Source e2e secrets management tool), Create-T3-App(Best way to create a Typesafe Web-App) and Elewa(An Open Source Chat-bot editor and manager for conversational learning platforms)",
+    answer: "Currently contributing to Infisical(an Open Source e2e secrets management tool), Create-T3-App(Best way to create a Typesafe Web-App) and Elewa(An Open Source Chat-bot editor and manager for conversational learning platforms).",
   },
   {
     question: "What is your favourite web framework?",
-    answer: "NextJs has got to be my favourite framework. The ability to decide the rendering pattern that's best for your app and the amazing list of libraries build by the community around the framework make it really easy to build full-stack typesafe and fast webapps, plus the new changes introduced in Next13 with server components will make the rendering patterns even more easy and intuitive. I also like Angular, Sveltekit and Astro",
+    answer: "NextJs has got to be my favourite framework. The ability to decide the rendering pattern that's best for your app and the amazing list of libraries build by the community around the framework make it really easy to build full-stack typesafe and fast webapps, plus the new changes introduced in Next13 with server components will make the rendering patterns even more easy and intuitive. I also like Angular, Sveltekit and Astro.",
   },
   {
     question: "Thoughts on Angular?",
-    answer: "I don't think Angular gets enough appreciation. I've had the privilege of building complex apps with it and the set of options that you get straight out the gate and the prescribed architecture that it comes with make it an amazing options for building enterprise level apps (bonus:powerful CLI)",
+    answer: "I don't think Angular gets enough appreciation. I've had the privilege of building complex apps with it and the set of options that you get straight out the gate and the prescribed architecture that it comes with make it an amazing options for building enterprise level apps (bonus:powerful CLI).",
   },
 ]
